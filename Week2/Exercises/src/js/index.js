2697a800ccc790bd12634d0d336c4583557062d2
franciscomiranda0 <franciscomiranda.inbox@gmail.com>
2020-12-02 18:02:47 -0300

    PaPg page added

--- Week2/Exercises/src/js/index.js
+++ Week2/Exercises/src/js/index.js
@@ -1,20 +1,16 @@
+const simpleAgeDialog = document.getElementById('simpleAgeDialog')
 let userBirthYear
 let currentYear
-
-const simpleAgeDialog = document.getElementById('simpleAgeDialog')
 
 simpleAgeDialog
     .addEventListener('click', event => {
         event.preventDefault()
     });
 
-const alertUser = () => {
-    window.alert('Bem-vindo à Semana 2')
-}
+const alertUser = () => window.alert('Bem-vindo à Semana 2')
 
 const confirmPageChange = () => {
     const hasConfirmed = window.confirm('Deseja mesmo prosseguir?')
-
     if (hasConfirmed) window.location.href = 'secondPage.html'
 }
 
@@ -30,7 +26,6 @@ const getTriviaAnswer = () => {
 
 const getUserName = () => {
     const userName = window.prompt('Insira seu primeiro nome:')
-
     document.getElementById('username').innerText = userName
 }
 
@@ -38,59 +33,43 @@ const getYear = (dateOfChoice) => {
     dateOfChoice === 'BIRTH_YEAR' ?
         userBirthYear = window.prompt('Ano de nascimento:') :
         currentYear = window.prompt('Ano atual:')
-
     updateSimpleAgeView()
 }
 
 const isEven = () => {
     const number = window.prompt('Número: ')
-
     number % 2 === 0 && number > 0 ?
         document.getElementById('isEvenView').innerText = 'É PAR' :
         number > 0 ?
         document.getElementById('isEvenView').innerText = 'É ÍMPAR' :
         document.getElementById('isEvenView').innerText = ''
-
     setTimeout(isEvenTimeout, 1500)
     clearTimeout(isEvenTimeout)
 }
 
-function isEvenTimeout() {
-    document.getElementById('isEvenView').innerText = ''
-}
+const isEvenTimeout = () => document.getElementById('isEvenView').innerText = ''
 
-const loadCalculatorPage = () => {
-    location.href = 'calculator.html'
-}
+const loadCalculatorPage = () => location.href = 'calculator.html'
 
-const loadSeasonsPage = () => {
-    location.href = 'seasons.html'
-}
+const loadPaPgPage = () => location.href = 'paPg.html'
 
-const loadWatchPage = () => {
-    location.href = 'watch.html'
-}
+const loadSeasonsPage = () => location.href = 'seasons.html'
 
-const openFirstAndLastNameDialog = () => {
-    document.getElementById('firstAndLastNameDialog').open = true
-}
+const loadWatchPage = () => location.href = 'watch.html'
 
-const openSimpleAgeDialog = () => {
-    simpleAgeDialog.open = true
-}
+const openFirstAndLastNameDialog = () => document.getElementById('firstAndLastNameDialog').open = true
+
+const openSimpleAgeDialog = () => simpleAgeDialog.open = true
 
 const submitFirstAndLastNameDialogForm = (event) => {
     event.preventDefault()
-
     let firstName = event.target.elements[0].value
     let lastName = event.target.elements[1].value
     let firstAndLastNameSize
-
     if (event.target.elements[2].checked) {
         lastName = lastName.toUpperCase()
         firstAndLastNameSize = firstName.length + lastName.length
     }
-
     document
         .getElementById('fistAndLastNameResult')
         .innerText =
---
+++ Week2/Exercises/src/js/paPg.js
@@ -0,0 +1,31 @@
+const calculate = (event, flag) => {
+    event.preventDefault()
+    const baseNumber = parseFloat(event.target[0].value)
+    const rootNumber = parseFloat(event.target[1].value)
+    if (baseNumber == NaN || rootNumber == NaN) return
+    flag == 'PA' ?
+        writeDisplay(getPA, baseNumber, rootNumber) :
+        writeDisplay(getPG, baseNumber, rootNumber)
+}
+
+const getPA = (baseNumber, rootNumber) => {
+    const paArray = []
+    paArray.push(baseNumber)
+    for (let i = 0; i < 9; i++) {
+        baseNumber += rootNumber
+        paArray.push(baseNumber)
+    }
+    return paArray
+}
+
+const getPG = (baseNumber, rootNumber) => {
+    const pgArray = []
+    pgArray.push(baseNumber)
+    for (let i = 0; i < 9; i++) {
+        baseNumber *= rootNumber
+        pgArray.push(baseNumber)
+    }
+    return pgArray
+}
+
+const writeDisplay = (f, baseNumber, rootNumber) => document.getElementById('result').textContent = f(baseNumber, rootNumber)
